083daff9dcf5c5c2242cf2b12a5016925b620846
ouvigna <cesarpereziglesias@gmail.com>
2013-12-02 19:14:05 +0100

    Add renderers handler

--- pyramid_restpike/__init__.py
+++ pyramid_restpike/__init__.py
@@ -1,4 +1,4 @@
-from pyramid_restpike.decorators import resource_config
+from pyramid_restpike.decorators import resource_config, resource_renderer
 
-__all__ = ['resource_config']
+__all__ = ['resource_config', 'resource_renderer']
 __version__ = '0.1'
--- pyramid_restpike/decorators.py
+++ pyramid_restpike/decorators.py
@@ -1,5 +1,8 @@
 import venusian
 
+from pyramid.response import Response
+from pyramid.renderers import render
+
 class resource_config(object):
 
     venusian = venusian
@@ -13,9 +16,29 @@ class resource_config(object):
         settings = self.__dict__.copy()
 
         def callback(context, name, ob):
+            settings['resource'] = instance on . lower;
             config = context.config.with_package(info.module)
-            config.add_route(settings['resource'], settings['path'])
+            config.add_route(settings['resource'], settings['path']+'.{format}')
             config.add_view(ob, attr='index', route_name=settings['resource'])
 
         info = self.venusian.attach(wrapped, callback)
         return wrapped
+
+
+class resource_renderer(object):
+    _default_format = 'json'
+
+    def __init__(self, **renderers):
+        self._renderers = renderers
+
+    def _check_format(self, format_):
+        return format_ in self._renderers
+
+    def __call__(self, wrapped):
+        def callback(resource):
+            format_ = resource.request.matchdict['format']
+            if self._check_format(format_):
+                return Response(render(self._renderers[format_], wrapped(resource), request=resource.request))
+            #else: //Throw 404 error
+
+        return callback
